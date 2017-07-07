9e5d99d804a947c876d98f543a45d5f6c1768d3e
kresta <kresta@debian.home>
2017-07-07 17:10:37 +0300

    update

--- Vagrantfile
+++ Vagrantfile
@@ -65,8 +65,8 @@ Vagrant.configure("2") do |config|
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
    config.vm.provision "shell", inline: <<-SHELL
-   rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+   rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm   
    yes | yum -y install puppet
-   puppet apply /vagrant/default.pp
+   sudo puppet apply /vagrant/default.pp
    SHELL
 end
--- default.pp
+++ default.pp
@@ -1,6 +1,64 @@
-file { '/root/example_file.txt':
-    ensure => "file",
-    owner  => "root",
-    group  => "root",
-    mode   => "700",
-    content => "Congratulations!  Puppet has created this file."}
+file { 'puppet.conf':
+    path    => '/etc/puppet/puppet.conf',
+    ensure  => file,
+    content => "[main]
+    # The Puppet log directory.
+    # The default value is '$vardir/log'.
+    logdir = /var/log/puppet
+
+    # Where Puppet PID files are kept.
+    # The default value is '$vardir/run'.
+    rundir = /var/run/puppet
+
+    # Where SSL certificates are kept.
+    # The default value is '$confdir/ssl'.
+    ssldir = $vardir/ssl
+    
+    [master]
+    autosign = true
+    [agent]
+    # The file in which puppetd stores a list of the classes
+    # associated with the retrieved configuratiion.  Can be loaded in
+    # the separate ``puppet`` executable using the ``--loadclasses``
+    # option.
+    # The default value is '$confdir/classes.txt'.
+    classfile = $vardir/classes.txt
+
+    # Where puppetd caches the local configuration.  An
+    # extension indicating the cache format is added automatically.
+    # The default value is '$confdir/localconfig'.
+    localconfig = $vardir/localconfig",
+    mode    => 0644,
+    owner   => root,
+    group   => root
+}
+
+package { 'epel-release':
+    ensure => latest,
+    name   => 'epel-release'
+}
+
+package { 'nginx':
+    ensure => latest,
+    name   => 'nginx',
+    require => Package['epel-release']
+}
+
+package { 'fabric':
+    ensure => latest,
+    name   => 'fabric',
+    require => Package['epel-release']
+}
+
+package { 'vim-enhanced':
+    ensure => latest,
+    name   => 'vim-enhanced'
+}    
+
+service { 'nginx.service':
+    ensure    => running,
+    enable    => true,
+    name      => 'nginx',
+#    subscribe => File['nginx.conf'],
+    require   => Package['nginx']
+}
